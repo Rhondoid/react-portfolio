36a6c7ab70b5babbd4b755e8c90cbefab6245fa0
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-20 16:57:23 -0700

    added components files, folders, css, colors

---
+++ my-app/src/components/contact/contact.jsx
@@ -0,0 +1,65 @@
+import { React, Form, Field, ErrorMessage } from 'react';
+
+function ContactForm() {
+  return (
+    <React
+      initialValues={{ name: '', email: '', message: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = 'Required';
+        }
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.message) {
+          errors.message = 'Required';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="div" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <Field as="textarea" name="message" />
+            <ErrorMessage name="message" component="div" />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </React>
+  );
+}
+
+// import ContactForm from './ContactForm';
+
+// function ContactPage() {
+//   return (
+//     <div>
+//       <h2>Contact Us</h2>
+//       <ContactForm />
+//     </div>
+//   );
+// }
+
---
+++ my-app/src/components/resume/resume.jsx
@@ -0,0 +1,10 @@
+import './ResumePage.css';
+
+function ResumePage() {
+  return (
+    <div>
+      <h2>My Resume</h2>
+      <a href="path/to/resume.pdf" download className="resume-link">Download Resume</a>
+    </div>
+  );
+}
